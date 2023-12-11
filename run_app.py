344edd921818657cc31c04eac5b3f435faf01033
Emmanuel manucho <manuchoadero@gmail.com>
2023-12-11 17:12:59 +0300

    minor changes to app

--- run_app.py
+++ run_app.py
@@ -2,7 +2,7 @@
 
 from config import create_app
 
+app = create_app()
 
 if __name__ == "__main__":
-    app = create_app()
-    app.run(port='5000', host="0.0.0.0")
+    app.run(port="5000", host="0.0.0.0")
